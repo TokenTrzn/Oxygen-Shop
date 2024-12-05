f12c2c8494b8c1933c9fa65576a0c66b6b915b53
Alberto <albertogilsanjuan@gmail.com>
2024-12-05 13:25:33 +0100

    ADD: Pop Up Component & FIX: Return To Top Icon

--- scripts/popUp.js
+++ scripts/popUp.js
@@ -1,36 +1,41 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const closeBtn = document.getElementById('exitIcon')
-    const popUp = document.getElementById('popUp')
-    const popUpSendBtn = document.getElementById('popUp-btn')
-    const emailInput = document.getElementById('email-input')
 
-    let isPopUpActive = false
+const popUp = document.getElementById('popUp')
+const closeBtn = document.getElementById('exitIcon')
+const popUpSendBtn = document.getElementById('popUp-btn')
+const emailInput = document.getElementById('email-input')
+const overlay = document.getElementById('overlay')
 
-    const showPopUp = () => {
-        if (!isPopUpActive && !localStorage.getItem('popUpClosed')) {
-            popUp.classList.add('show')
-            isPopUpActive = true
-        }
+let isPopUpActive = false
+localStorage.removeItem('popUpClosed')
+
+const showPopUp = () => {
+    if (!isPopUpActive && !localStorage.getItem('popUpClosed')) {
+        popUp.classList.add('show')
+        overlay.classList.add('show')
+        isPopUpActive = true
     }
+}
 
-    const closePopUp = () => {
-        popUp.classList.remove('show')
-        localStorage.setItem('popUpClosed', 'true')
+const closePopUp = () => {
+    popUp.classList.remove('show')
+    overlay.classList.remove('show')
+    localStorage.setItem('popUpClosed', 'true')
+}
+
+setTimeout(showPopUp, 5000)
+
+const showPopUpOnScroll = () => {
+    const scrollPosition = window.scrollY + window.innerHeight
+    const pageHeight = document.documentElement.scrollHeight
+
+    if (scrollPosition >= pageHeight * 0.25) {
+        showPopUp()
+        window.removeEventListener('scroll', showPopUpOnScroll)
     }
+}
 
-    setTimeout(showPopUp, 5000)
-
-    const showPopUpOnScroll = () => {
-        const scrollPosition = window.scrollY + window.innerHeight
-        const pageHeight = document.documentElement.scrollHeight
-
-        if (scrollPosition >= pageHeight * .25) {
-            showPopUp()
-            window.removeEventListener('scroll', showPopUpOnScroll)
-        }
-    }
-
-    closeBtn.addEventListener('click', closePopUp)
-    window.addEventListener('scroll', showPopUpOnScroll)
+window.addEventListener('scroll', showPopUpOnScroll)
+closeBtn.addEventListener('click', closePopUp)
 
 })
